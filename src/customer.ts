4a91603fc11fb9dbb5531a9a2b9127bd38cebbd6
Leticia Moreira <leticia.moreira147@hotmail.com>
2025-01-26 22:53:56 -0300

    Values Objects

---
+++ src/customer.ts
@@ -0,0 +1,42 @@
+import Address from "./address";
+
+class Customer {
+    _id: string;
+    _name: string;
+    _address!: Address;
+    _active: boolean = true;
+
+    constructor(id: string, name: string, address: string) {
+        this._id = id;
+        this._name = name;
+        this.validade();
+    }
+
+    validade() {
+        if (this._name === '') {
+            throw new Error('Name is required');
+        }
+
+        if (this._id === '') {
+            throw new Error('Id is required');
+        }
+    }
+
+    changeName(name: string) {
+        this._name = name;
+    }
+
+    activate() {
+        if(this._address !== undefined) {
+            throw new Error('Address is required');
+        }
+        this._active = true;
+    }
+
+    deactive() {
+        this._active = false;
+    }
+    setAddress(address: Address) {
+        this._address = address;
+    }
+}
