fc846e62ed2ac1df839646234a853036f4ad62ae
Leticia Moreira <leticia.moreira147@hotmail.com>
2025-02-01 18:05:54 -0300

    order

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -5,6 +5,7 @@ export default class Customer {
     private _name: string;
     private _address!: Address;
     private _active: boolean = false
+    private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -45,9 +46,18 @@ export default class Customer {
         this._active = false;
     }
     
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
+    }
+
     set Address(address: Address) {
         this._address = address;
     }
     get name (): string { return this._name; }
 
+    get id (): string { return this._id; }
 }
--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,12 +1,24 @@
+import Customer from "../entity/customer";
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
 import OrderService from "./order.service";
+import { v4 as uuid } from "uuid";
 
 describe("Order service unit tests", () => {
+    it("should place new order", () => {
+        const customer = new Customer("c1", "Customer 1");
+        const item1 = new OrderItem("i1", "Item 1", 10, "p1", 1);
+
+        const order = OrderService.placeOrder(customer, [item1]);
+
+        expect(customer.rewardPoints).toBe(5);
+        expect(order.total()).toBe(10);
+
+    });
+
     it("should get total order", () => {
         const item1 = new OrderItem("1", "Item 1", 100, "p1", 1);
         const item2 = new OrderItem("2", "Item 2", 200, "p2", 2);
-
 
         const order1 = new Order("o1", "c1", [item1])
         const order2 = new Order("o2", "c1", [item2])
