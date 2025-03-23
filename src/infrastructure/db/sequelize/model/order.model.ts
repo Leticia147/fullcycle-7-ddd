7faa46b460c8a712343eedc8bbb53112764f4c7d
Leticia Moreira <leticia.moreira147@hotmail.com>
2025-03-23 20:52:17 -0300

    tests

--- src/infrastructure/db/sequelize/model/order.model.ts
+++ src/infrastructure/db/sequelize/model/order.model.ts
@@ -21,7 +21,7 @@ export default class OrderModel extends Model {
 
   @ForeignKey(() => CustomerModel)
   @Column({ allowNull: false })
-  declare customerId: string;
+  declare customer_id: string;
 
   @BelongsTo(() => CustomerModel)
   declare customer: CustomerModel;
--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -8,7 +8,7 @@ export default class OrderRepository implements OrderRepositoryInterface {
     async create(entity: Order): Promise<void> {
         await OrderModel.create({
             id: entity.id,
-            customerId: entity.customerId,
+            customer_id: entity.customerId,
             total: entity.total(),
             items: entity.items.map((item) => ({
                 id: item.id,
@@ -25,15 +25,28 @@ export default class OrderRepository implements OrderRepositoryInterface {
     }
    
     async update(entity: Order): Promise<void> {
+        const items = entity.items.map((item) => ({
+            id: item.id,
+            name: item.name,
+            price: item.price,
+            product_id: item.productId,
+            quantity: item.quantity,
+            order_id: entity.id,
+        }));
         await OrderModel.update(
             {
-                customerId: entity.customerId,
                 total: entity.total(),
             },
             {
                 where: { id: entity.id },
-            }
+            },
+            
         );
+
+        await OrderItemModel.destroy({
+            where: { order_id: entity.id },
+        });
+        await OrderItemModel.bulkCreate(items);
     }
 
     async find(id: string): Promise<Order> {
@@ -42,11 +55,9 @@ export default class OrderRepository implements OrderRepositoryInterface {
             include: [{model: OrderItemModel}],
         },);
 
-        
-
         return new Order(
             orderModel.id,
-            orderModel.customerId,
+            orderModel.customer_id,
             orderModel.items.map((item) => new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity))
         );
     }
@@ -58,7 +69,7 @@ export default class OrderRepository implements OrderRepositoryInterface {
 
         return orderModels.map((orderModel) => new Order(
             orderModel.id,
-            orderModel.customerId,
+            orderModel.customer_id,
             orderModel.items.map((item) => new OrderItem(item.id, item.name, item.price, item.product_id, item.quantity))
         ));
     }
